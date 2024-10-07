fa57b83b6e4be247a3940bdadbecf7d6e72fe8f7
Trung Do <dothanhtrung@pm.me>
2024-10-07 18:22:34 +0700

    Transition sequence

--- src/lib.rs
+++ src/lib.rs
@@ -63,7 +63,10 @@ impl Plugin for UiEffectPluginNoState {
     }
 }
 
-pub trait TransformSequence {}
+pub trait TransformSequence {
+    fn animated(&self, transform: &mut Transform, time_delta_ms: u128);
+    fn is_finished(&self) -> bool;
+}
 
 #[derive(Default)]
 pub struct NextTransform {
@@ -77,44 +80,27 @@ pub struct ScaleSequence {
     pub phase: usize,
     pub displayed_time_ms: u128,
     pub sequence: Vec<NextTransform>,
+    pub sequence_gap: Vec<NextTransform>,
 }
-
-impl TransformSequence for ScaleSequence {}
 
 #[derive(Default)]
-pub enum Mode {
-    #[default]
-    Once,
-    Repeat,
+pub struct TransitionSequence {
+    pub phase: usize,
+    pub displayed_time_ms: u128,
+    pub sequence: Vec<NextTransform>,
 }
 
-#[derive(Component, Default)]
-pub struct UiEffect {
-    pub scale_sequence: ScaleSequence,
-    pub mode: Mode,
-}
-
-fn animate(mut commands: Commands, mut query: Query<(&mut UiEffect, &mut Transform, Entity)>, time: Res<Time>) {
-    for (mut effect, mut transform, e) in query.iter_mut() {
-        if effect.scale_sequence.phase >= effect.scale_sequence.sequence.len() {
-            match effect.mode {
-                Mode::Repeat => effect.scale_sequence.phase = 0,
-                Mode::Once => {
-                    commands.entity(e).remove::<UiEffect>();
-                    continue;
-                }
-            }
-        }
-
-        let target_scale = &effect.scale_sequence.sequence[effect.scale_sequence.phase];
+impl TransformSequence for ScaleSequence {
+    fn animated(&mut self, transform: &mut Transform, time_delta_ms: u128) {
+        let target_scale = &self.sequence[self.phase];
         if transform.scale == target_scale.value {
-            if effect.scale_sequence.displayed_time_ms >= target_scale.display_time_ms {
-                effect.scale_sequence.phase += 1;
-                effect.scale_sequence.displayed_time_ms = 0;
+            if self.displayed_time_ms >= target_scale.display_time_ms {
+                self.phase += 1;
+                self.displayed_time_ms = 0;
             } else {
-                effect.scale_sequence.displayed_time_ms += time.delta().as_millis();
+                self.displayed_time_ms += time_delta_ms;
             }
-            continue;
+            return;
         }
 
         let sign_x = if transform.scale.x < target_scale.value.x {
@@ -132,7 +118,7 @@ fn animate(mut commands: Commands, mut query: Query<(&mut UiEffect, &mut Transfo
         } else {
             -1.
         };
-        let time_delta = time.delta().as_millis() as f32;
+        let time_delta = time_delta_ms as f32;
         transform.scale.x += sign_x * target_scale.speed * time_delta;
         transform.scale.y += sign_y * target_scale.speed * time_delta;
         transform.scale.z += sign_z * target_scale.speed * time_delta;
@@ -146,5 +132,90 @@ fn animate(mut commands: Commands, mut query: Query<(&mut UiEffect, &mut Transfo
         if sign_z * transform.scale.z > sign_z * target_scale.value.z {
             transform.scale.z = target_scale.value.z;
         }
+    }
+
+    fn is_finished(&self) -> bool {
+        self.phase >= self.sequence.len()
+    }
+}
+
+impl TransformSequence for TransitionSequence {
+    fn animated(&mut self, transform: &mut Transform, time_delta_ms: u128) {
+        let target_transition = &self.sequence[self.phase];
+        if transform.translation == target_transition.value {
+            if self.displayed_time_ms >= target_transition.display_time_ms {
+                self.phase += 1;
+                self.displayed_time_ms = 0;
+            } else {
+                self.displayed_time_ms += time_delta_ms;
+            }
+            return;
+        }
+
+        let sign_x = if transform.translation.x < target_transition.value.x {
+            1.
+        } else {
+            -1.
+        };
+        let sign_y = if transform.translation.y < target_transition.value.y {
+            1.
+        } else {
+            -1.
+        };
+        let sign_z = if transform.translation.z < target_transition.value.z {
+            1.
+        } else {
+            -1.
+        };
+        let time_delta = time_delta_ms as f32;
+        transform.translation.x += sign_x * target_transition.speed * time_delta;
+        transform.translation.y += sign_y * target_transition.speed * time_delta;
+        transform.translation.z += sign_z * target_transition.speed * time_delta;
+
+        if sign_x * transform.translation.x > sign_x * target_transition.value.x {
+            transform.translation.x = target_transition.value.x;
+        }
+        if sign_y * transform.translation.y > sign_y * target_transition.value.y {
+            transform.translation.y = target_transition.value.y;
+        }
+        if sign_z * transform.translation.z > sign_z * target_transition.value.z {
+            transform.translation.z = target_transition.value.z;
+        }
+    }
+
+    fn is_finished(&self) -> bool {
+        self.phase >= self.sequence.len()
+    }
+}
+
+#[derive(Default)]
+pub enum Mode {
+    #[default]
+    Once,
+    Repeat,
+}
+
+#[derive(Component, Default)]
+pub struct UiEffect {
+    pub scale_sequence: ScaleSequence,
+    pub transition_sequece: TransitionSequence,
+    pub mode: Mode,
+}
+
+fn animate(mut commands: Commands, mut query: Query<(&mut UiEffect, &mut Transform, Entity)>, time: Res<Time>) {
+    for (mut effect, mut transform, e) in query.iter_mut() {
+        if effect.scale_sequence.is_finished() && effect.transition_sequece.is_finished() {
+            match effect.mode {
+                Mode::Repeat => effect.scale_sequence.phase = 0,
+                Mode::Once => {
+                    commands.entity(e).remove::<UiEffect>();
+                    continue;
+                }
+            }
+        }
+
+        let time_delta_ms = time.delta().as_millis();
+        effect.scale_sequence.animated(&mut transform, time_delta_ms);
+        effect.transition_sequece.animated(&mut transform, time_delta_ms);
     }
 }
